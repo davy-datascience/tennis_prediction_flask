1757329b7f425af574d7d9f474144bcfcef56ef8
davy-datascience <davy.datascience@gmail.com>
2021-02-08 00:02:41 +0000

    Fix player2 in bold for scheduled matches

--- tennis/classes/MatchResult.py
+++ tennis/classes/MatchResult.py
@@ -21,7 +21,8 @@ class Match:
             self.p2_s4_gms = row["p2_s4_gms"]
             self.p2_s5_gms = row["p2_s5_gms"]
 
-            self.p1_wins = row["p1_wins"]
+            if row["p1_wins"] is not None:
+                self.p1_wins = row["p1_wins"]
 
 
 class MatchResult:
